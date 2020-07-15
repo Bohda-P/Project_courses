8e5277be637057fb68fc35d4c4e6bfbfd308a2a9
Bohdan <bogdanpaseka2000@gmail.com>
2020-07-15 22:07:19 +0300

    Add cards of  courses and develop basket of courses

--- models/course.js
+++ models/course.js
@@ -1,4 +1,6 @@
-const { v4: uuidv4 } = require('uuid');
+const {
+    v4: uuidv4
+} = require('uuid');
 const fs = require('fs')
 const path = require('path')
 
@@ -8,36 +10,58 @@ class Course {
         this.price = price
         this.img = img
         this.id = uuidv4();
+
     }
 
-    toJSON(){
-        return{
+    toJSON() {
+        return {
             title: this.title,
             price: this.price,
             img: this.img,
             id: this.id
         }
     }
+    static async update(course) {
+        const courses = await Course.getAll()
+
+        const idx = courses.findIndex(c => c.id === course.id)
+        courses[idx] = course
+
+        return new Promise((resolve, reject) => {
+            fs.writeFile(
+                path.join(__dirname, '..', 'data', 'courses.json'),
+                JSON.stringify(courses),
+                (err) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve()
+                    }
+
+                }
+            )
+        })
+    }
 
     async save() {
         const courses = await Course.getAll()
         courses.push(this.toJSON())
 
-        return new Promise((resolve, reject)=>{
+        return new Promise((resolve, reject) => {
             fs.writeFile(
                 path.join(__dirname, '..', 'data', 'courses.json'),
-               JSON.stringify(courses),
-                (err)=>{
-                    if (err){
-                        reject (err)
-                    }else{
+                JSON.stringify(courses),
+                (err) => {
+                    if (err) {
+                        reject(err)
+                    } else {
                         resolve()
                     }
-    
+
                 }
             )
         })
-        
+
     }
 
     static getAll() {
@@ -57,6 +81,10 @@ class Course {
             )
         })
 
+    }
+    static async getById(id) {
+        const courses = await Course.getAll()
+        return courses.find(c => c.id === id)
     }
 }
 
